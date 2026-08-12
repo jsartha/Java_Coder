5a829883e87069620af41992dac45df72bd18882
Sarthak Joshi <jsarthak168@gmail.com>
2026-08-12 18:30:19 +0530

    update inheritance

---
+++ Inheritance/single.java
@@ -0,0 +1,47 @@
+package Inheritance;
+
+public class single {
+
+    public static void main(String[] args) {
+
+        car c = new car();
+
+        c.color = "Red";
+
+        System.out.println("Color of car is: " + c.color);
+
+        c.start();
+        c.drive();
+        c.hork();
+        c.stop();
+    }
+}
+
+class vehicle {
+
+    String brand;
+    String color;
+    int speed;
+
+    void start() {
+        System.out.println("Vehicle is started");
+    }
+
+    void stop() {
+        System.out.println("Vehicle is stopped");
+    }
+}
+
+class car extends vehicle {
+
+    int number_of_doors;
+    String fuel_type;
+
+    void drive() {
+        System.out.println("Car is driving");
+    }
+
+    void hork() {
+        System.out.println("Car is honking");
+    }
+}
