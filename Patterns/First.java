abbc9a8117e9f575f0342a08e514676cebd5564e
Sarthak Joshi <jsarthak168@gmail.com>
2026-08-13 20:27:03 +0530

    add pattern number code

--- Patterns/First.java
+++ Patterns/First.java
@@ -1,4 +1,6 @@
 package Patterns;
+
+import java.util.Scanner;
 class First {
 
     public static void main(String args []){
@@ -11,7 +13,23 @@ class First {
             System.out.println();
 
         }
-           
+   
+        
+        Scanner sc= new Scanner (System.in);
 
+        
+        System.out.println(" enter the number of row");
+        int row=sc.nextInt();
+       System.out.println(" enter the number of col");
+        int col=sc.nextInt();
+
+       for(int i=1 ; i<=row;i++){
+
+        for(int j=1 ; j<=col;j++){
+
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+       }
 }
 }
