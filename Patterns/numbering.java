9c992774694b6dab3e4c81fe4b9b3b75333c8ba2
Sarthak Joshi <jsarthak168@gmail.com>
2026-08-13 23:19:59 +0530

    add  numbering

--- Patterns/numbering.java
+++ Patterns/numbering.java
@@ -11,5 +11,16 @@ public class numbering {
             }
             System.out.println();
         }
+
+
+
+        for(int i=1; i<=5; i++){
+
+            for(int j=1; j<=5; j++){
+
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
     }
 }
